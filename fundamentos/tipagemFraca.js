f49cd1f786d42be88ee713d896bb5a8d642e09a9
Marcello <marcello.caon@4effect.com.br>
2018-08-12 23:54:20 -0300

    Tipagem Fraca

---
+++ fundamentos/tipagemFraca.js
@@ -0,0 +1,13 @@
+let qualquer = 'legal';
+console.log(qualquer);
+console.log(typeof qualquer);
+
+qualquer = 3.43434;
+console.log(qualquer);
+console.log(typeof qualquer);
+
+// Evitar nomes genericos e siglas
+let valor = '';
+let numero = 1;
+
+// JavaScript possui TipagemFraca, diferente do Java que ja possui Tipagem Forte
