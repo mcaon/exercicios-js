0226a76b918d6dfc906bbac5ecfaf6fd514ed1f7
Marcello <marcello.caon@4effect.com.br>
2019-02-11 21:23:40 -0200

    Objetos

---
+++ objeto/criandoObjetos.js
@@ -0,0 +1,61 @@
+// usando a notacao literal
+const obj1 = {};
+console.log(obj1);
+
+
+// Object em JS
+console.log(typeof Object, typeof new Object());
+
+const obj2 = new Object;
+console.log(obj2);
+
+
+
+// Funcoes CONSTRUTORAS -> preco e desc estao apenas dentro da funcao
+function Produto(nome, preco, desc) {
+    // nome pode ser acessado por fora
+    this.nome = nome;
+    this.getPrecoComDesconto = () => {
+        return preco * (1 - desc)
+    };
+
+}
+
+const p1 = new Produto('caneta', 7.99, 0.15);
+const p2 = new Produto('Notebook', 2998.99, 0.25);
+console.log(p1.getPrecoComDesconto(), p2.getPrecoComDesconto())
+
+
+
+
+// Funcao FACTORY (padrao de projeto) => fabrica um objeto
+function criarFuncionario(nome, salarioBase, faltas) {
+
+    return {
+        nome,
+        salarioBase,
+        faltas,
+        getSalario() {
+            return (salarioBase / 30) *(30 - faltas)
+        }
+
+    }
+}
+
+const f1 = criarFuncionario('Joao', 7980, 4);
+const f2 = criarFuncionario('Maria', 11400, 1);
+console.log(f1.getSalario(), f2.getSalario());
+
+
+
+//  Object.create
+const filha = Object.create(null);
+filha.nome = 'Ana'
+console.log(filha)
+
+
+
+
+// Uma funcao famosa que retorna Objeto...
+const fromJSON = JSON.parse('{"info": "Sou um JSON"}');
+console.log(fromJSON.info);
---
+++ objeto/oo.js
@@ -0,0 +1,25 @@
+// CODIGO NAO EXECUTAVEL
+
+// Procedural
+// funcoes manipulam dados
+processamento(valor1, valor2, valor3)
+
+// OO
+// objetos com valores (atributos) e funcoes
+// dados tem funcoes
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        // ...
+    }
+};
+
+objeto.processamento() // foco passou a ser o OBJETO
+
+// Principios importantes:
+// 1. abstração
+// 2. encapsulamento
+// 3. herança (prototype)
+// 4. polimorfismo
